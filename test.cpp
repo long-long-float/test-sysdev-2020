500170fb385e0e2cdda4f4d2317b0e01ea29dcc4
long-long-float <niinikazuki@yahoo.co.jp>
2020-11-07 02:52:23 +0900

    Print elements with index

--- test.cpp
+++ test.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main(int argc, char** argv) {
   int ary[] = {1, 2, 3, 4};
 
+  int i = 0;
   for (auto &e : ary) {
-    cout << e << endl;
+    cout << i << ": " << e << endl;
+    i++;
   }
 
   return 0;
